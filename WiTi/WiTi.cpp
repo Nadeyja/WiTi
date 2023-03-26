6bba59de2b1089bfe61e4aee4ec9f0a5fa5f05e4
Nadeyja <nadia.hamerlak@gmail.com>
2023-03-26 14:29:27 +0200

    In progress

--- WiTi/WiTi.cpp
+++ WiTi/WiTi.cpp
@@ -2,21 +2,60 @@
 #include <chrono>
 #include <fstream>
 #include <iomanip>
+#include <bitset>
 using std::cout;
 using std::cin;
 using namespace std;
 
-int value_in_time(int W, int T, int time) {
-    return W * (time - T);
+int value_in_time(int w, int t, int time) {
+    return w * (time - t);
+}
+string value_of_number(int x) {
+    string binary = std::bitset<20>(x).to_string();
+    int y = 65;
+    string result = "";
+    string k = "";
+    for (int i = binary.size()-1; i >= 0; i--) {
+        if (binary[i] == '1') {
+            k = (char)y;
+            result += k;
+        }
+        y++;
+    }
+    return(result);
+}
+int int_of_string(string s) {
+    int result = 0;
+    int h;
+    for (int i = 0;i < s.size(); i++) {
+        h = (int)s[i]-64;
+        result += 2 ^ h;
+    }
+    return result;
 }
 int algorithm(int P[], int W[], int T[], int N) {
-    int a = N;
-    int R[pow(2,a)];
+    int a = pow(2,N);
+    int * R;
+    int c;
+    R = new int[a];
+    R[0]=0;
+    int t = 0;
+    int h = 10000;
+    string z = "";
+    string act = "";
+    for (int i = 1; i < a;i++) {
+        z = value_of_number(i);
+        for (int j = 0; j < z.size(); j++) {
+            act = z[j];
+            t = int_of_string(z) - int_of_string(act) + value_in_time();
+            c = value_in_time(W[j],T[j],)
+        }
+    }
 }
 
 int main()
 {
-    auto start = chrono::high_resolution_clock::now();
+    /*auto start = chrono::high_resolution_clock::now();
     int sumOfC, m, C, N, P[100], W[100], T[100], * X;
     ifstream f("data.txt");
     string s;
@@ -40,6 +79,9 @@ int main()
     double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 
     time_taken *= 1e-9;
-    cout << fixed << time_taken << setprecision(9) << " seconds";
+    cout << fixed << time_taken << setprecision(9) << " seconds";*/
+    int x;
+    cin >> x;
+    cout << value_of_number(x);
     return 0;
 }
